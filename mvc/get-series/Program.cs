24ba61532dce4df167caa672345cb48cd272d4b7
tamar-avivi <tamar.avivi.24.2@gmail.com>
2024-02-01 21:27:51 +0200

    runtime compilation

--- mvc/get-series/Program.cs
+++ mvc/get-series/Program.cs
@@ -8,9 +8,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+// builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) {
